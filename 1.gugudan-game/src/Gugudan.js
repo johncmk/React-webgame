b7d1f8d1249dd02b0984851d114deb4a953c57b0
johnck <johnck408@gmail.com>
2024-01-26 21:16:20 -0500

    Added onRefInputFocus method

--- 1.gugudan-game/src/Gugudan.js
+++ 1.gugudan-game/src/Gugudan.js
@@ -14,17 +14,54 @@ class Gugudan extends Component
         };
     }
 
+    onSubmit = (e) =>
+    {
+        e.preventDefault();
+        if (parseInt(this.state.value) === this.state.first * this.state.second)
+        {
+            this.setState((prevState) => {
+                return {
+                    result: `정답: ${prevState.value}`,
+                    first: Math.ceil(Math.random() * 9),
+                    second: Math.ceil(Math.random() * 9),
+                    value: '',
+                };
+            });
+            this.inputNumber.focus();
+        }
+        else
+        {
+            this.setState({
+                result: '땡',
+                value: '',
+            });
+            this.inputNumber.focus();
+        }
+    }
+
+    onChange = (e) =>
+    {
+        this.setState({ value: e.target.value })
+    };
+
+    onRefInputFocus = (c) =>
+    {
+        this.inputNumber = c
+    }
+
+    inputNumber;
+
     render()
     {
         return (
-            <div>
+            <>
                 <div>{this.state.first} 곱하기 {this.state.second}는?</div>
-                <form onSubmit={(e)}>
-                    <input type="number" value={this.state.value} onChange={(e) => this.setState({value: e.target.value})}/>
+                <form onSubmit={this.onSubmit}>
+                    <input ref={this.onRefInputFocus} type="number" value={this.state.value} onChange={this.onChange}/>
                     <button>입력!</button>
                 </form>
                 <div>{this.state.result}</div>
-            </div>  
+            </>  
         );
     }
 }
